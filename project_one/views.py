ed912a8ecf29117ed53b1c7b20d32448c384628c
Hafizur Rahman <hafizur15-1979@diu.edu.bd.com>
2022-04-03 14:42:14 +0600

    class7

--- project_one/views.py
+++ project_one/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render,  HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1> hello django </h1>") 
+def about(reqest):
+    return HttpResponse('i am a developer')
