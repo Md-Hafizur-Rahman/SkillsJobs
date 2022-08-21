b936394d92e2ca32dd441ced617df5318d02df39
Hafizur Rahman <hafizur15-1979@diu.edu.bd.com>
2022-08-21 19:12:54 +0600

    cartadd

---
+++ ecom/apps/frontend/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+from apps.frontend.models import HomeSlider
+
+
+class HomeView(TemplateView):
+    template_name = "frontend/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['slider_list'] = HomeSlider.objects.all()
+        return context
+
+class ShopView(TemplateView):
+    template_name = "frontend/shop.html"
+
+class ContactView(TemplateView):
+    template_name = "frontend/contact.html"
